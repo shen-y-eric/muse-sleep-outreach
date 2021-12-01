11059cc6d3be8bab93ac1d821937d051e48bb25d
shen.y.eric <70082457+shen-y-eric@users.noreply.github.com>
2021-12-01 15:50:31 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,62 @@
+let songs = [
+    {
+        name: "I'm Yours",
+        path: "mraz.mp3",
+        artist: 'Jason Mraz',
+        cover: 'mraz.jpg'
+    },
+    {
+        name: "Stay",
+        path: "stay.mp3",
+        artist: "Rihanna",
+        cover: "stay.jpeg"
+    },
+    {
+        name: "Put Your Records On",
+        path: "records.mp3",
+        artist: "Corinne Bailey Rae",
+        cover: "records.jpeg"
+    },
+    {
+        name: "She Will Be Loved",
+        path: "maroon.mp3",
+        artist: "Maroon 5",
+        cover: "maroon.jpg"
+    },
+    {
+        name: "Someone Like You",
+        path: "adele.mp3",
+        artist: "Adele",
+        cover: "adele.jpeg"
+    },
+    {
+        name: "Thinking out Loud",
+        path: "sheeran.mp3",
+        artist: "Ed Sheeran",
+        cover: "sheeran.png"
+    },
+    {
+        name: "I Just Called To Say I Love You",
+        path: "wonder.mp3",
+        artist: "Stevie Wonder",
+        cover: "wonder.jpeg"
+    },
+    {
+        name: "Make You Feel My Love",
+        path: "love_adele.mp3",
+        artist: "Adele",
+        cover: "19.jpg"
+    },
+    {
+        name: "Let Her Go",
+        path: "passenger.mp3",
+        artist: "Passenger",
+        cover: "passenger.jpeg"
+    },
+    {
+        name: "Memories",
+        path: "Memories.mp3",
+        artist: "Maroon 5",
+        cover: "memories.jpeg"
+    }
+]
